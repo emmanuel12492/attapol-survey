83c9b9b7868bcad82bbbb4b0569d804d7be34845
emmanuel12492 <emmanuel12492@github.com>
2025-09-18 17:01:53 +0300

    Fix deployment issues: - Remove typo in survey form - Disable Turbopack in production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   experimental: {
-    // Only enable Turbopack in development
-    turbo: process.env.NODE_ENV === 'development'
+    // Disable Turbopack for now
+    turbo: false
   }
 }
